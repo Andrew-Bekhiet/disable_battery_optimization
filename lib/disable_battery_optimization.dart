ef51f3bcd0ac5c3a7959c1fa73050dc86430a510
Ali <aliammarali125@gmail.com>
2021-03-06 17:48:11 +0300

    migrate p;gin

--- lib/disable_battery_optimization.dart
+++ lib/disable_battery_optimization.dart
@@ -8,22 +8,22 @@ class DisableBatteryOptimization {
       const MethodChannel('disable_battery_optimization');
 
   static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
+    final String version =  ( await _channel.invokeMethod<String>('getPlatformVersion'))!;
     return version;
   }
 
   static Future<bool> isIgnoringBatteryOptimizations() async {
-    final reading = await _channel.invokeMethod('isIgnoringBatteryOptimizations');
+    final reading = (await _channel.invokeMethod<bool>('isIgnoringBatteryOptimizations'))!;
     return reading;
   }
 
   static Future<String> openBatteryOptimizationSettings() async {
-    final reading = await _channel.invokeMethod('openBatteryOptimizationSettings');
+    final reading = (await _channel.invokeMethod<String>('openBatteryOptimizationSettings'))!;
     return reading;
   }
 
   static Future<String> stopOptimizingBatteryUsage() async {
-    final reading = await _channel.invokeMethod('stopOptimizingBatteryUsage');
+    final reading = (await _channel.invokeMethod<String>('stopOptimizingBatteryUsage'))!;
     return reading;
   }
 
